5190954a1c912aad197e668fa9b58938d437059c
Nico Rieck <nico.rieck@gmail.com>
2017-05-13 22:50:58 +0200

    Make UseExplorerStyle property usable by ListView

---
+++ Source/PresentationTheme.Aero/ThemeOptions.cs
@@ -0,0 +1,43 @@
+﻿namespace PresentationTheme.Aero
+{
+    using System.Windows;
+    using System.Windows.Controls;
+
+    public static class ThemeOptions
+    {
+        #region public bool UseExplorerStyle { get; set; }
+
+        /// <summary>
+        ///   Identifies the UseExplorerStyle attached dependency property.
+        /// </summary>
+        public static readonly DependencyProperty UseExplorerStyleProperty =
+            DependencyProperty.RegisterAttached(
+                "UseExplorerStyle",
+                typeof(bool),
+                typeof(ThemeOptions),
+                new FrameworkPropertyMetadata(
+                    false,
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.Inherits));
+
+        /// <summary>
+        ///   Gets a flag indicating whether the <see cref="Control"/> uses the
+        ///   Explorer style.
+        /// </summary>
+        public static bool GetUseExplorerStyle(Control d)
+        {
+            return (bool)d.GetValue(UseExplorerStyleProperty);
+        }
+
+        /// <summary>
+        ///   Sets a flag indicating whether the <see cref="Control"/> uses the
+        ///   Explorer style.
+        /// </summary>
+        public static void SetUseExplorerStyle(Control d, bool value)
+        {
+            d.SetValue(UseExplorerStyleProperty, value);
+        }
+
+        #endregion
+    }
+}
---
+++ Source/PresentationTheme.Aero/TreeViewItemLevelConverter.cs
@@ -0,0 +1,63 @@
+﻿namespace PresentationTheme.Aero
+{
+    using System;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+
+    public class TreeViewItemLevelConverter : DependencyObject, IValueConverter
+    {
+        private static readonly Lazy<TreeViewItemLevelConverter> LazyInstance =
+            new Lazy<TreeViewItemLevelConverter>(() => new TreeViewItemLevelConverter());
+
+        public static TreeViewItemLevelConverter Instance => LazyInstance.Value;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var item = value as TreeViewItem;
+            if (item == null)
+                return DependencyProperty.UnsetValue;
+
+            int level = GetItemLevel(item);
+            TreeViewService.SetItemLevel(item, level);
+
+            if (parameter is int)
+                return level * (int)parameter;
+
+            if (parameter is double)
+                return level * (double)parameter;
+
+            return level;
+        }
+
+        public object ConvertBack(
+            object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        private int GetItemLevel(TreeViewItem item)
+        {
+            var parent = GetParentItem(item);
+            if (parent != null)
+                return TreeViewService.GetItemLevel(parent) + 1;
+            return 0;
+        }
+
+        private static TreeViewItem GetParentItem(TreeViewItem item)
+        {
+            DependencyObject obj = item;
+            for (obj = obj.GetVisualOrLogicalParent(); obj != null;
+                 obj = obj.GetVisualOrLogicalParent()) {
+                var parentItem = obj as TreeViewItem;
+                if (parentItem != null)
+                    return parentItem;
+                if (obj is TreeView)
+                    break;
+            }
+
+            return null;
+        }
+    }
+}
