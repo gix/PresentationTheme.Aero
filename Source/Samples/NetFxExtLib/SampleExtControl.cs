81d69a3f9a4e5858f42448ed4bd44cc688f2a2ad
Nico Rieck <nico.rieck@gmail.com>
2019-05-25 21:23:04 +0200

    Rename samples to emphasize the intended framework to be used

---
+++ Source/Samples/NetFxExtLib/SampleExtControl.cs
@@ -0,0 +1,15 @@
+namespace NetFxExtLib
+{
+    using System.Windows;
+    using System.Windows.Controls;
+
+    public class SampleExtControl : Control
+    {
+        static SampleExtControl()
+        {
+            var forType = typeof(SampleExtControl);
+            DefaultStyleKeyProperty.OverrideMetadata(
+                forType, new FrameworkPropertyMetadata(forType));
+        }
+    }
+}
---
+++ Source/Samples/NetFxLib/SampleControl.cs
@@ -0,0 +1,15 @@
+namespace NetFxLib
+{
+    using System.Windows;
+    using System.Windows.Controls;
+
+    public class SampleControl : Control
+    {
+        static SampleControl()
+        {
+            var forType = typeof(SampleControl);
+            DefaultStyleKeyProperty.OverrideMetadata(
+                forType, new FrameworkPropertyMetadata(forType));
+        }
+    }
+}
